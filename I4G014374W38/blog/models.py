7caf5d55aed976d807352aa45b222d8ac5da0da1
Jude <judetochyokoye@gmail.com>
2022-06-20 15:20:02 +0100

    completed

--- I4G014374W38/blog/models.py
+++ I4G014374W38/blog/models.py
@@ -1,3 +1,11 @@
+from tkinter import CASCADE
+from turtle import ondrag
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    pub_date = models.DateTimeField(auto_now_add=True)

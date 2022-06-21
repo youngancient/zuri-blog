7099c1ffc974b7d8880f063dc697faa7c0aa8926
Jude <judetochyokoye@gmail.com>
2022-06-21 08:44:33 +0100

    edited Post model

--- I4G014374W38/blog/models.py
+++ I4G014374W38/blog/models.py
@@ -1,11 +1,12 @@
 from tkinter import CASCADE
 from turtle import ondrag
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_date = models.DateTimeField()
+    author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    created_date = models.DateTimeField(timezone.now)
     pub_date = models.DateTimeField(auto_now_add=True)
